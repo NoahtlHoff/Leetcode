420634ca5875a2da49e87bf94e1f7a6ac9f0599f
NoahH Laptop <NoahH@HPLAPTOP>
2024-11-18 11:56:27 +0100

    RomantoInt

--- Leetcode/Challenges/Program.cs
+++ Leetcode/Challenges/Program.cs
@@ -4,8 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            string[] strs = ["eat", "tea", "tan", "ate", "nat", "bat"];
+            string s = "III";
         }
     }
 }
